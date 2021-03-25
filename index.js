050c3c745183ceeeb345837e61f6dd8bd212bba8
Michael-amali <michael.acheampon@amalitech.org>
2021-03-25 07:12:39 +0000

    Modified the mini-pic to change the slider

--- index.js
+++ index.js
@@ -63,3 +63,112 @@ function showCart(){
 
 }
 
+
+
+// js of aj
+
+
+//stores previously clicked images properties
+let previous;
+// showImage function
+function changeImage(event){
+      let imageSrc  = $(event).attr("src");
+      let currentDiv;
+      
+      // console.log(imageSrc);
+
+      
+      if(String(imageSrc) === String("/images/mini_coat_front.jpg")){
+           
+
+        $(".mySlides .img1").attr("src","/images/coat_pic_front.jpg");
+        let man_1 = $(".mySlides .img1").attr("src");
+        // console.log(man_1);
+
+        $(".mySlides .img2").attr("src","/images/kid_model.jpg");
+        $(".mySlides .img3").attr("src","/images/coat_pic_back.jpg");
+
+            currentDiv = $(event).parent().addClass("borderAdd");
+
+
+
+           if(typeof previous === 'undefined'){
+              previous = currentDiv;
+           }else{
+            previous.removeClass("borderAdd");
+            previous = currentDiv;
+           }
+           
+      }else if(String(imageSrc) === String("/images/mini_kid_model.jpg")){
+
+            $(".mySlides .bigProductImg").attr("src","/images/kid_model.jpg");
+           
+             currentDiv =$(event).parent().addClass("borderAdd");
+        
+             
+
+
+
+             if(typeof previous == 'undefined'){
+                previous = currentDiv;
+             }else{
+              previous.removeClass("borderAdd");
+              previous = currentDiv;
+             }
+             
+
+      }else if(String(imageSrc) === String("/images/mini_coat_back.jpg")){
+        $(".mySlides .bigProductImg").attr("src","/images/coat_pic_back.jpg");
+        
+        currentDiv =   $(event).parent().addClass("borderAdd");
+
+        if(typeof previous == 'undefined'){
+            previous = currentDiv;
+         }else{
+          previous.removeClass("borderAdd");
+          previous = currentDiv;
+         }
+         
+      }
+
+    
+      changeDetails(event);
+}
+
+
+//function to change product details starts here
+function changeDetails(event){
+  // console.log(event)
+
+  imageSrc = $(event).attr("src");
+
+  if(String(imageSrc) === String("/images/coat_pic_front.jpg")){
+      //name of product
+      $("h3 .reduced_1").text("Uk made Shoes");
+      //price on dicount
+      $(".money_div").text(180.00);
+      //previous  price
+      $("p span del").text(280.99);
+     
+  }
+  else if(String(imageSrc) === String("/images/kid_model.jpg")){
+      
+       //name of product
+       $("#text h2").text("Corporate Office Suits");
+       //price on dicount
+       $(".delivery-div h2 #price").text(580.00);
+       //previous  price
+       $(".delivery-div h2 #oldprice").text(780.99);
+
+  }else if(String(imageSrc) === String("/images/coat_pic_back.jpg")){
+      //name of product
+      $("#text h2").text("Boy's Cardigan with Zip");
+        
+      //price on dicount
+      $(".delivery-div h2 #price").text(120.00);
+      //previous  price
+      $(".delivery-div h2 #oldprice").text(200.99);
+
+  }
+  
+}
